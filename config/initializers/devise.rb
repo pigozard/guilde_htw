4f38068c688d4120a0072e752482a6b51009f359
Pierre Gozard <pierre.gozard03@gmail.com>
2026-03-10 20:16:13 +0100

    fix: use verified Gmail as Devise mailer sender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'a488b8001@smtp-brevo.com'
+  config.mailer_sender = 'pierre.gozard03@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
